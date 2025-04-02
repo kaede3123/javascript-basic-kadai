b4ecd3d922036ebdbfb3addb684b79ab1958f92b
kaede3123 <kaede19911203@gmail.com>
2025-04-02 23:44:46 +0900

    kadai_mdn_jsを追加

--- kadai_mdn_js/mdn.js
+++ kadai_mdn_js/mdn.js
@@ -1,7 +1,7 @@
 const date = new Date();
 const [month, day, year] = [
-    date.getMonth(),
+    date.getMonth() +1,
     date.getDate(),
     date.getFullYear(),
 ];
-console.log(year + '年', month + '月', day + '日')
+console.log(year+'年',month+'月',day+'日')
