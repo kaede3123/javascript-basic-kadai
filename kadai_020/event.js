6d81d5426af60391819c46f04575e960d01b9c7f
kaede3123 <kaede19911203@gmail.com>
2025-04-07 18:32:16 +0900

    kadai_020を追加

--- kadai_020/event.js
+++ kadai_020/event.js
@@ -1,5 +1,5 @@
 const btn = document.getElementById('btn')
+const text = document.getElementById('text')
 btn.addEventListener('click', () => {
-    const text = document.getElementById('text')
     text.textContent = 'ボタンをクリックしました'
 })
